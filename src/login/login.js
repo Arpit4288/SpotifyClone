72805019eb5b7a6054a1dfc107142451e9ef8e56
Arpit <arpitkumardhuriya4288@gmail.com>
2022-11-16 15:24:24 +0530

    changes related to login and env files

---
+++ src/login/login.js
@@ -0,0 +1,58 @@
+import { ACCESS_TOKEN, EXPIRES_IN, TOKEN_TYPE } from "../common";
+
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;  // use meta to import data from env file
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+const APP_URL = import.meta.env.VITE_APP_URL;
+// console.log("Client id : " + CLIENT_ID);
+// console.log("Redirect url : " + REDIRECT_URI);
+// console.log("App url : " + APP_URL);
+const scopes = "user-top-read user-follow-read playlist-read-private user-library-read";
+const authorizeUser = ()=>{
+    const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scope=${scopes}&show_dialog=true`;
+    window.open(url, "login", "width = 800, height = 600");
+    
+}
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginButton = document.getElementById("login-to-spotify");
+    loginButton.addEventListener("click", authorizeUser)
+})
+
+window.setItemsInLocalStorage = ({accessToken, tokenType, expires_in})=>{
+    localStorage.setItem(ACCESS_TOKEN, accessToken);
+    localStorage.setItem(TOKEN_TYPE, tokenType);
+    localStorage.setItem(EXPIRES_IN, expires_in);
+    // window.location.href = `${APP_URL}/dashboard/dashboard.html`;
+    window.location.href = APP_URL;
+}
+
+window.addEventListener("load", () =>{
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if(accessToken){
+        window.location.href = `${APP_URL}/dashboard/dashboard.html`;
+    }
+    if(window.opener != null && !window.opener?.closed){
+        // this piece of code identifies wheather they are inside the pop-up or not
+        window.focus();
+        //login page is closed by the user or any of this thing is happen
+        if(window.location.href.includes("error")){
+            window.close();
+        }
+
+        const {hash} = window.location;
+        const searchParams = new URLSearchParams(hash);
+        const accessToken = searchParams.get("#access_token");
+
+        // '#access_token=BQA5xv48NFRxTQadJ00q5bxehncCcfh0x3f5PacpptUGeHVR6-xfWJ90IweHLw2BBXHl12Mt0FQlzxv4rxJi0-WFWEniSa3qMDEuEBSMEbROsva_c9ZW6foNz64K3lqNvcnBJwgViQXToUIKpGkcjVgc6Zbi_R7hiWJocU6qBt-1Xp0cSNVM6_1cvOws_zn694EfTewFo2Unwll-KnVtZRxY1n5wv2_s7Q&token_type=Bearer&expires_in=3600'
+
+        const tokenType = searchParams.get("token_type");
+        const expires_in = searchParams.get("expires_in");
+        if(accessToken){
+            window.close();
+            // we want to go back to opener of this popup
+            window.opener.setItemsInLocalStorage({accessToken, tokenType, expires_in});
+        }
+        else{
+            window.close();
+        }
+    }
+})
